33f3949135616ea174971cc729f5a078301a93ab
Harry Leach <harry@pluscosmic.dev>
2026-01-16 18:17:42 +0000

    Refactoring

---
+++ Nucleus.Clips/ApexLegends/ApexEndpoints.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Nucleus.Clips.ApexLegends;
+
+public static class ApexEndpoints
+{
+    public static void MapApexEndpoints(this WebApplication app)
+    {
+        RouteGroupBuilder group = app.MapGroup("apex-legends");
+        group.MapGet("map-rotation", async (MapService mapService) => await mapService.GetMapRotation())
+            .WithName("GetApexMapRotation");
+        group.MapPost("assign-account", AssignAccount).WithName("AssignAccount");
+    }
+
+    // Platform can be PS4, X1, PC
+    public static Results<Ok, BadRequest> AssignAccount(string username, string platform)
+    {
+        return TypedResults.Ok();
+    }
+}
